815f29c25877a50ee1e6aeb6732311d418b0cb1d
Karl Steltenpohl <madchops@Karls-MBP.local>
2020-06-09 01:37:06 -0500

    ...

---
+++ assets/app.js
@@ -0,0 +1,21 @@
+
+
+$(function () {
+
+    $('.table-type-1').DataTable({
+        "paging": false,
+        "searching": false
+    });
+
+
+
+
+    // init
+    var t = setTimeout(function () {
+
+    }, 900);
+
+
+
+});
+

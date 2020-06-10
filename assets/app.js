05a71217a64c2820ac0746c87ab27457ea42c36c
Karl Steltenpohl <madchops@Karls-MBP.local>
2020-06-10 04:06:52 -0500

    ...

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,56 @@
 
+let minDateFilter = "2020";
+let maxDateFilter = "2021";
 
 $(function () {
 
     $('.table-type-1').DataTable({
         "paging": false,
-        "searching": false
+        "searching": false,
+        "bInfo": false
+    });
+
+    // let policyTable = $('.table-type-policy').DataTable({
+    //     "paging": false,
+    //     "searching": false,
+    //     "bInfo": false
+    // });
+
+    $("#datepicker").datepicker({
+        "changeYear": true,
+        //"dateFormat": "yy",
+        "onSelect": function (date) {
+            //minDateFilter = new Date(date).getTime();
+            //policyTable.fnDraw();
+        }
+    }).change(function () {
+        minDateFilter = new Date(this.value).getTime();
+        //policyTable.fnDraw();
+    });
+
+    $("#datepicker-from").datepicker({
+        "changeYear": true,
+        //"dateFormat": "yy",
+        "onSelect": function (date) {
+            //minDateFilter = new Date(date).getTime();
+            //policyTable.fnDraw();
+        }
+    }).change(function () {
+        minDateFilter = new Date(this.value).getTime();
+        //policyTable.fnDraw();
+    });
+
+    $("#datepicker-to").datepicker({
+        "changeYear": true,
+
+        //"dateFormat": "yy",
+        "onSelect": function (date) {
+            //maxDateFilter = new Date(date).getTime();
+            //policyTable.fnDraw();
+        }
+    }).change(function () {
+        maxDateFilter = new Date(this.value).getTime();
+        //policyTable.fnDraw();
     });
 
 

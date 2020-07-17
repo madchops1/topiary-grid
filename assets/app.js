7a41ac5edcaa79156e0e587bb8b89bc573af4b4c
Karl Steltenpohl <madchops@Karls-MBP.local>
2020-07-17 12:10:42 -0500

    new updates

--- assets/app.js
+++ assets/app.js
@@ -4,17 +4,19 @@ let maxDateFilter = "2021";
 
 $(function () {
 
+    $(document).tooltip();
+
     $('.table-type-1').DataTable({
         "paging": false,
         "searching": false,
         "bInfo": false
     });
 
-    // let policyTable = $('.table-type-policy').DataTable({
-    //     "paging": false,
-    //     "searching": false,
-    //     "bInfo": false
-    // });
+    let policyTable = $('.table-type-policy').DataTable({
+        "paging": false,
+        "searching": false,
+        "bInfo": false
+    });
 
     $("#datepicker").datepicker({
         "changeYear": true,
@@ -32,6 +34,8 @@ $(function () {
         "changeYear": true,
         //"dateFormat": "yy",
         "onSelect": function (date) {
+            console.log('BETA: on select');
+
             //minDateFilter = new Date(date).getTime();
             //policyTable.fnDraw();
         }
@@ -45,6 +49,8 @@ $(function () {
 
         //"dateFormat": "yy",
         "onSelect": function (date) {
+            console.log('ALPHA: on select');
+
             //maxDateFilter = new Date(date).getTime();
             //policyTable.fnDraw();
         }
@@ -54,6 +60,21 @@ $(function () {
     });
 
 
+    // Tooltip
+    /*$(document).tooltip({
+        position: {
+            my: "center bottom-20",
+            at: "center top",
+            using: function (position, feedback) {
+                $(this).css(position);
+                $("<div>")
+                    .addClass("arrow")
+                    .addClass(feedback.vertical)
+                    .addClass(feedback.horizontal)
+                    .appendTo(this);
+            }
+        }
+    });*/
 
 
     // init
